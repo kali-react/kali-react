edbaed92f324e1802189575481b0426323efe383
a <1379.arman@gmail.com>
2022-10-20 17:07:31 -0400

    last

--- packages/terminal/src/Footer.tsx
+++ packages/terminal/src/Footer.tsx
@@ -5,7 +5,7 @@ interface ButtonProps {
   isSmall?: boolean;
 }
 
-const Button = styled.button(({ variant, isSmall }: ButtonProps) => [
+export const Button = styled.button(({ variant, isSmall }: ButtonProps) => [
   // The common button styles
   tw`px-8 py-2 rounded focus:outline-none transform duration-75`,
 
--- packages/terminal/src/index.tsx
+++ packages/terminal/src/index.tsx
@@ -1,2 +1,3 @@
+export { Button } from "./Footer";
 export { ReduxButton } from "./Footer";
 export { ReduxProvider } from "./redux/provider";
